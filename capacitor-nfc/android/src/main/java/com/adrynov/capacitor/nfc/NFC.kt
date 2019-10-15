679067ac0e1748ea3fbdda02cd68f5d24e3d45b3
Andrei Drynov <andrei@globetech.ie>
2019-10-15 17:47:25 +0100

    NFC messages and tech lists

--- capacitor-nfc/android/src/main/java/com/adrynov/capacitor/nfc/NFC.kt
+++ capacitor-nfc/android/src/main/java/com/adrynov/capacitor/nfc/NFC.kt
@@ -5,9 +5,11 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.content.IntentFilter
 import android.nfc.NfcAdapter
+import android.nfc.tech.NfcF
 import android.util.Log
 import com.getcapacitor.*
 import java.util.*
+
 
 /**
  * Capacitor plugin that access NFC capabilities of a device.
@@ -16,11 +18,29 @@ import java.util.*
 class NFC : Plugin() {
     private val TAG = NFC::class.java.simpleName
 
+    private val PERMISSION_NOT_SET = Manifest.permission.NFC + " permission is not set in AndroidManifest.xml"
+
     private val NFC_REQUEST_PERMISSIONS = 9020
 
-    private var nfcAdapter: NfcAdapter? = null
+    private val NDEF = "ndef"
+    private val NDEF_MIME = "ndef-mime"
+    private val NDEF_FORMATABLE = "ndef-formatable"
 
-    private val nfcAvailable: Boolean = this.nfcAdapter != null
+    // Message constants
+    private val MESSAGE_OK = "OK"
+    private val MESSAGE_NO_NFC = "Device does not support NFC"
+    private val MESSAGE_NFC_DISABLED = "Unable to use NFC, user denied request"
+    private val MESSAGE_UNABLE_TO_PROCESS_TAG = "Unable to process NFC tag"
+    private val EVENT_TAG_DISCOVERED = "TAG_DISCOVERED"
+    private val EVENT_TECH_DISCOVERED = "TECH_DISCOVERED"
+    private val EVENT_NDEF_DISCOVERED = "NDEF_DISCOVERED"
+    private val EVENT_NDEF_FORMATTABLE_DISCOVERED = "NDEF_FORMATTABLE_DISCOVERED"
+
+    private val STATUS_NFC_OK = "ok"
+    private val STATUS_NO_NFC = "none"
+    private val STATUS_NFC_DISABLED = "disabled"
+
+    private var nfcAdapter: NfcAdapter? = null
 
     internal var watchingCalls: Map<String, PluginCall> = HashMap()
 
@@ -70,8 +90,8 @@ class NFC : Plugin() {
             return
         }
 
-        if (!nfcAvailable) {
-            call.error("NFC unavailable")
+        if (this.nfcAdapter != null) {
+            call.error(MESSAGE_NO_NFC)
             return
         }
 
@@ -83,33 +103,43 @@ class NFC : Plugin() {
     }
 
     private fun enableForegroundDispatch() {
-        if (!nfcAvailable) {
-            Log.e(TAG, "NFC is not available");
+        if (this.nfcAdapter == null) {
+            Log.e(TAG, MESSAGE_NO_NFC);
             return
         }
+        // other apps should not be able to intercept NFC while our app is active.
+        val activity = this.bridge.activity;
 
-        val activity = this.bridge.getActivity();
+        val intent = Intent(context, activity.javaClass).apply {
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
 
-        val intent = Intent(context, activity.javaClass)
-        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
-
+        // The Android system can populate it with the details of the tag when it is scanned.
         val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
 
-        val filters = arrayOfNulls<IntentFilter>(1)
+        // Handle the intents that you want to intercept: TAG_DISCOVERED, TECH_DISCOVERED and/or NDEF_DISCOVERED
+        val intentFilters = arrayOfNulls<IntentFilter>(1)
+
+        // Supported NFC technologies
         val techList = arrayOf<Array<String>>()
 
-        filters[0] = IntentFilter()
+        intentFilters[0] = IntentFilter()
 
-        with(filters[0]) {
+        with(intentFilters[0]) {
             this?.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED)
             this?.addCategory(Intent.CATEGORY_DEFAULT)
             try {
+                /* Handles all MIME based dispatches. You should specify only the ones that you need. */
                 this?.addDataType("text/plain")
             } catch (ex: IntentFilter.MalformedMimeTypeException) {
                 throw RuntimeException("Check your MIME type")
             }
         }
 
-        nfcAdapter?.enableForegroundDispatch(activity, pendingIntent, filters, techList)
+        // TODO:  obtain the class of the technology that you want to support.
+        val techListsArray = arrayOf(arrayOf<String>(NfcF::class.java.name))
+
+
+        nfcAdapter?.enableForegroundDispatch(activity, pendingIntent, intentFilters, techList)
     }
 }
