4a5817aa20d1e397032b51c1ca5eaa6daf4e384f
Andrei Drynov <andrei@globetech.ie>
2019-10-15 15:18:37 +0100

    Starting from scratch

--- capacitor-nfc/CapacitorNfc.podspec
+++ capacitor-nfc/CapacitorNfc.podspec
@@ -1,13 +1,13 @@
 
   Pod::Spec.new do |s|
     s.name = 'CapacitorNfc'
-    s.version = '0.0.1'
-    s.summary = 'Read and write NFC tags'
+    s.version = '0.1.0'
+    s.summary = 'Read NFC tags'
     s.license = 'MIT'
     s.homepage = 'https://github.com/adrynov/Capacitor-NFC-Plugin'
     s.author = 'Andrei Drynov'
     s.source = { :git => 'https://github.com/adrynov/Capacitor-NFC-Plugin', :tag => s.version.to_s }
-    s.source_files = 'ios/Plugin/Plugin/**/*.{swift,h,m,c,cc,mm,cpp}'
-    s.ios.deployment_target  = '10.0'
+    s.source_files = 'ios/Plugin/**/*.{swift,h,m,c,cc,mm,cpp}'
+    s.ios.deployment_target  = '11.0'
     s.dependency 'Capacitor'
   end
--- capacitor-nfc/src/web.ts
+++ capacitor-nfc/src/web.ts
@@ -1,66 +1,23 @@
 import { WebPlugin } from '@capacitor/core';
-import { NFCPlugin, NfcStatus, NfcOptions } from './definitions';
-// import { uuid4 } from './utils';
+import { NFCPlugin } from './definitions';
 
-// declare var navigator: any;
+export class NFCWeb extends WebPlugin implements NFCPlugin {
+  constructor() {
+    super({
+      name: 'NFC',
+      platforms: ['web']
+    });
+  }
 
-export class NFCPluginWeb extends WebPlugin implements NFCPlugin {
-
-    constructor() {
-        super({
-            name: 'NFC',
-            platforms: ['web']
-        });
-    }
-
-    startScan(options: NfcOptions): Promise<void> {
-        console.log('NFC options', options);
-        return Promise.resolve();
-    }
-
-    stopScan(): Promise<void> {
-        return Promise.resolve();
-    }
-
-    getStatus(): Promise<NfcStatus | any> {
-        return Promise.resolve('none');
-    }
-
-    // getTagInfo(): Promise<NfcTag> {
-    //     return Promise.resolve({
-    //         tagId: this.getTagIt(),
-    //         manufacturer: navigator.vendor,
-    //         techList: ['web'],
-    //         type: 'TAG_DISCOVERED'
-    //     });
-    // }
-
-    showSettings(): Promise<void> {
-        return Promise.resolve();
-    }
-
-    // private getTagId() {
-    //     let uid = window.localStorage.getItem('_capuid');
-    //     if (uid) {
-    //         return uid;
-    //     }
-
-    //     uid = uuid4();
-    //     window.localStorage.setItem('_capuid', uid);
-    //     return uid;
-    // }
+  async echo(options: { value: string }): Promise<{value: string}> {
+    console.log('ECHO', options);
+    return options;
+  }
 }
 
-const NFC = new NFCPluginWeb();
+const NFC = new NFCWeb();
 
 export { NFC };
 
-
-// export class NFCWeb extends WebPlugin implements  {
-
-//   async echo(options: { value: string }): Promise<{value: string}> {
-//     console.log('ECHO', options);
-//     return Promise.resolve({ value: options.value });
-//   }
-// }
-
+import { registerWebPlugin } from '@capacitor/core';
+registerWebPlugin(NFC);
