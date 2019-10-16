004079e4b106be4562e787021c97a59be394252e
Andrei Drynov <andrei@globetech.ie>
2019-10-16 20:42:03 +0100

    Kotlin coroutine support

--- capacitor-nfc/android/proguard-rules.pro
+++ capacitor-nfc/android/proguard-rules.pro
@@ -19,3 +19,16 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+# https://github.com/Kotlin/kotlinx.coroutines/blob/master/kotlinx-coroutines-core/jvm/resources/META-INF/proguard/coroutines.pro
+
+# ServiceLoader support
+-keepnames class kotlinx.coroutines.internal.MainDispatcherFactory {}
+-keepnames class kotlinx.coroutines.CoroutineExceptionHandler {}
+-keepnames class kotlinx.coroutines.android.AndroidExceptionPreHandler {}
+-keepnames class kotlinx.coroutines.android.AndroidDispatcherFactory {}
+
+# Most of volatile fields are updated with AFU and should not be mangled
+-keepclassmembernames class kotlinx.** {
+    volatile <fields>;
+}
--- capacitor-nfc/android/src/main/java/com/adrynov/capacitor/nfc/NFC.kt
+++ capacitor-nfc/android/src/main/java/com/adrynov/capacitor/nfc/NFC.kt
@@ -1,21 +1,30 @@
-package com.adrynov.capacitor.nfc
-
 import android.Manifest
+import android.app.Activity
 import android.app.PendingIntent
 import android.content.Intent
 import android.content.IntentFilter
 import android.nfc.NfcAdapter
-import android.nfc.tech.NfcF
+import android.nfc.tech.NfcA
 import android.util.Log
 import com.getcapacitor.*
-import java.util.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
+private const val NFC_REQUEST_PERMISSION = 9089
 
 /**
  * Capacitor plugin that access NFC capabilities of a device.
+ *
+ * https://developer.android.com/guide/topics/connectivity/nfc/advanced-nfc#tag-tech
  */
-@NativePlugin(permissions = [Manifest.permission.NFC]) // permissionRequestCode = PluginRequestCodes.NFC
-class NFC : Plugin() {
+@NativePlugin(
+        permissions = [Manifest.permission.NFC],
+        permissionRequestCode = NFC_REQUEST_PERMISSION
+)
+class NFC : Plugin(), CoroutineScope by MainScope() {
+
     private val TAG = NFC::class.java.simpleName
 
     private val PERMISSION_NOT_SET = Manifest.permission.NFC + " permission is not set in AndroidManifest.xml"
@@ -28,7 +37,7 @@ class NFC : Plugin() {
 
     // Message constants
     private val MESSAGE_OK = "OK"
-    private val MESSAGE_NO_NFC = "Device does not support NFC"
+    private val MESSAGE_NO_NFC = "NFC is not support"
     private val MESSAGE_NFC_DISABLED = "Unable to use NFC, user denied request"
     private val MESSAGE_UNABLE_TO_PROCESS_TAG = "Unable to process NFC tag"
     private val EVENT_TAG_DISCOVERED = "TAG_DISCOVERED"
@@ -40,6 +49,7 @@ class NFC : Plugin() {
     private val STATUS_NO_NFC = "none"
     private val STATUS_NFC_DISABLED = "disabled"
 
+    // NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(getActivity());
     private var nfcAdapter: NfcAdapter? = null
 
     internal var watchingCalls: Map<String, PluginCall> = HashMap()
@@ -57,15 +67,24 @@ class NFC : Plugin() {
 
     override fun handleOnNewIntent(intent: Intent?) {
         super.handleOnNewIntent(intent)
+
+        if (intent != null) {
+            // process the data from the scanned NFC tag
+//            val tagFromIntent: Tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+//            Log.i(TAG, "Hello")
+        }
     }
 
     override fun handleOnPause() {
-        super.handleOnPause()
+        val callingActivity = this.bridge.activity
 
-        nfcAdapter?.disableForegroundDispatch(this.bridge.getActivity())
+        launch {
+            disableForegroundDispatch(callingActivity)
+        }
     }
 
     override fun handleOnStop() {
+        cancel() // cancel any coroutine jobs
         super.handleOnStop()
     }
 
@@ -84,13 +103,14 @@ class NFC : Plugin() {
      */
     @PluginMethod()
     fun isNfcEnabled(call: PluginCall) {
+        // Check NFC permission
         if (!hasPermission(Manifest.permission.NFC)) {
             saveCall(call)
             pluginRequestAllPermissions()
             return
         }
 
-        if (this.nfcAdapter != null) {
+        if (this.nfcAdapter == null) {
             call.error(MESSAGE_NO_NFC)
             return
         }
@@ -102,12 +122,13 @@ class NFC : Plugin() {
         return call.success(ret)
     }
 
+    // https://developer.android.com/guide/topics/connectivity/nfc/advanced-nfc#foreground-dispatch
     private fun enableForegroundDispatch() {
         if (this.nfcAdapter == null) {
             Log.e(TAG, MESSAGE_NO_NFC);
             return
         }
-        // other apps should not be able to intercept NFC while our app is active.
+
         val activity = this.bridge.activity;
 
         val intent = Intent(context, activity.javaClass).apply {
@@ -117,29 +138,35 @@ class NFC : Plugin() {
         // The Android system can populate it with the details of the tag when it is scanned.
         val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
 
-        // Handle the intents that you want to intercept: TAG_DISCOVERED, TECH_DISCOVERED and/or NDEF_DISCOVERED
-        val intentFilters = arrayOfNulls<IntentFilter>(1)
-
-        // Supported NFC technologies
-        val techList = arrayOf<Array<String>>()
-
-        intentFilters[0] = IntentFilter()
-
-        with(intentFilters[0]) {
-            this?.addAction(NfcAdapter.ACTION_NDEF_DISCOVERED)
-            this?.addCategory(Intent.CATEGORY_DEFAULT)
+        // Declare the intents that you want to intercept: TAG_DISCOVERED, TECH_DISCOVERED or NDEF_DISCOVERED
+        val ndef = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED).apply {
             try {
-                /* Handles all MIME based dispatches. You should specify only the ones that you need. */
-                this?.addDataType("text/plain")
-            } catch (ex: IntentFilter.MalformedMimeTypeException) {
-                throw RuntimeException("Check your MIME type")
+                /* Handles all MIME based dispatches.You should specify only the ones that you need. */
+                addDataType("*/*")
+            } catch (e: IntentFilter.MalformedMimeTypeException) {
+                throw RuntimeException("fail", e)
             }
         }
 
-        // TODO:  obtain the class of the technology that you want to support.
-        val techListsArray = arrayOf(arrayOf<String>(NfcF::class.java.name))
+        val intentFilters = arrayOf(ndef)
 
+        // Supported NFC technologies
+        val techList = arrayOf(
+//                arrayOf(MifareUltralight::class.java.name, Ndef::class.java.name, NfcA::class.java.name),
+//                arrayOf(MifareClassic::class.java.name, Ndef::class.java.name, NfcA::class.java.name),
+                arrayOf<String>(NfcA::class.java.name)
+        )
 
         nfcAdapter?.enableForegroundDispatch(activity, pendingIntent, intentFilters, techList)
+    }
+
+    private fun disableForegroundDispatch(callingActivity: Activity) {
+        callingActivity.runOnUiThread {
+            try {
+                nfcAdapter?.disableForegroundDispatch(callingActivity)
+            } catch (e: IllegalStateException) {
+                Log.w(TAG, e)
+            }
+        }
     }
 }
